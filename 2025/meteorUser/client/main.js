8042e7efe73c269ffa9c2c7f0e73c2e5fa36c1d0
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2025-11-08 12:05:30 +0800

    meteorUser Update

--- 2025/meteorUser/client/main.js
+++ 2025/meteorUser/client/main.js
@@ -15,15 +15,29 @@ import './main.html';
 // which is designed to load the "signIn" template in /main.html by default.
 Session.set('browseSession', 'signIn');
 
+// Configure the action taken when an email verification link is processed.
 Accounts.onEmailVerificationLink(
+  // onEmailVerificationLink runs a function that receives two types of
+  // information. "token" is the verification token taken directly from the
+  // verification link, and "done" runs a function when the verification
+  // process is complete, which is not used here.
   function(token, done) {
+    // After the processing verification link is initiated, use verifyEmail()
+    // to send the verification token to the server. A callback function is run
+    // to process the potential error messages sent back from the server.
     Accounts.verifyEmail(token, function(err) {
+      // Import account-module.js, so the templates in account-module.html
+      // are ready to be loaded depending on the results of the verification process.
       import('/import/client/account-module.js').then(
         function() {
+          // If there's any error message returned from the server...
           if(err) {
+            // Change the browse session to load the "verifyFailed" template.
             Session.set('browseSession', 'verifyFailed');
           }
+          // If there's no error message...
           else {
+            // Change the browse session to load the "verifySuccess" template.
             Session.set('browseSession', 'verifySuccess');
           }
         }
