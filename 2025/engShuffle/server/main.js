0f47503c319e805fee038e721bc61dcc48aef305
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2025-12-05 16:14:14 +0800

    Week 14 engShuffle Update

--- 2025/engShuffle/server/main.js
+++ 2025/engShuffle/server/main.js
@@ -1,3 +1,18 @@
+import { Mongo } from 'meteor/mongo';
+
+global.sentenceDB = new Mongo.Collection('sentenceDB');
+global.userProfileDB = new Mongo.Collection('userProfileDB');
+
 Meteor.startup(async function() {
-	
+	await sentenceDB.removeAsync({});
+	let fulltext = await Assets.getTextAsync('sentenceList.txt');
+	let sentences = fulltext.split('\r\n');
+	sentences.forEach(
+		function(sentence) {
+			let sentenceInfo = sentence.trim().split('/');
+			let words = sentenceInfo[1].split(' ');
+			let shuffledWords = _.shuffle(words);
+			sentenceDB.insertAsync({level: sentenceInfo[0], words: words, shuffledWords: shuffledWords});
+		}
+	);
 });
