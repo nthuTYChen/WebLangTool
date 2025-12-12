662ae4f344af5a0d2913b3e814f682a50e1b2f3d
nthuTYChen <chen.ty@mx.nthu.edu.tw>
2025-12-12 16:14:23 +0800

    Week 15 engShuffle update

--- 2025/engShuffle/client/main.js
+++ 2025/engShuffle/client/main.js
@@ -1,6 +1,18 @@
+import { ReactiveVar } from 'meteor/reactive-var';
+
 import './main.html';
 
 Session.setDefault('browseSession', 'frontPage');
+Session.setDefault('username', '');
+
+global.userProfileDB = new Mongo.Collection('userProfileDB');
+global.sentenceDB = new Mongo.Collection('sentenceDB');
+
+Template.body.onCreated(function() {
+   Tracker.autorun(function() {
+      Meteor.subscribe('userProfile', Session.get('username'));
+   });
+});
 
 Template.body.helpers(
    {
@@ -22,6 +34,107 @@ Template.frontPage.helpers(
       frontPageHeader2: function() {
          let header = ['S', 'h', 'u', 'f', 'f', 'l', 'e', '!'];
          return header;
+      }
+   }
+);
+
+Template.frontPage.events(
+   {
+      'click button': function() {
+         Session.set('browseSession', 'shuffleTest');
+         Session.set('username', 'TYC');
+         Meteor.call('registerUser');
+      }
+   }
+);
+
+Template.shuffleTest.onCreated(function() {
+   this.shuffledWords = new ReactiveVar([]);
+   this.orderedWords = new ReactiveVar([]);
+   this.sentenceLength = new ReactiveVar(10000);
+   this.sentenceID = new ReactiveVar('');
+   this.startTime = null;
+   this.done = false;
+   Tracker.autorun(function() {
+      let userProfile = userProfileDB.findOne();
+      Meteor.subscribe('sentencesByLevel', userProfile && userProfile.level);
+   });
+});
+
+Template.shuffleTest.helpers(
+   {
+      isComplete: function() {
+         let currentList = Template.instance().orderedWords.get();
+         let senLength = Template.instance().sentenceLength.get();
+         if(currentList.length === senLength) {
+            return true;
+         }
+         return false;
+      },
+      shuffledWords: function() {
+         let currentList = Template.instance().shuffledWords.get();
+         return currentList;
+      },
+      orderedWords: function() {
+         let currentList = Template.instance().orderedWords.get();
+         return currentList;
+      },
+      userProfile: function(key) {
+         let userProfileObj = userProfileDB.findOne();
+         return userProfileObj && userProfileObj[key];
+      }
+   }
+);
+
+Template.shuffleTest.events(
+   {
+      'click #start': function(event, instances) {
+         let allSentences = _.shuffle(sentenceDB.find({}).fetch());
+         let shuffledList = _.shuffle(allSentences[0].shuffledWords);
+         instances.shuffledWords.set(shuffledList);
+         instances.sentenceLength.set(shuffledList.length);
+         instances.sentenceID.set(allSentences[0]._id);
+         instances.startTime = new Date();
+         event.currentTarget.style.display = 'none';
+      },
+      'click section#shuffleZone > button': function(event, instances) {
+         let removedWord = event.currentTarget.innerText;
+         let currentShuffledList = instances.shuffledWords.get();
+         let newShuffledList = _.without(currentShuffledList, removedWord);
+         instances.shuffledWords.set(newShuffledList);
+         let newOrderedList = instances.orderedWords.get();
+         newOrderedList.push(removedWord);
+         instances.orderedWords.set(newOrderedList);
+         if(newShuffledList.length === 0) {
+            instances.done = true;
+            let currentTime = new Date();
+            let totalTime = currentTime.getTime() - instances.startTime.getTime();
+            let ansRecord = {
+               sentenceID: instances.sentenceID.get(),
+               username: Session.get('username'),
+               RT: totalTime,
+               answer: newOrderedList
+            };
+            console.log(ansRecord);
+            Meteor.callAsync('recordAns', ansRecord).then(function(correctness) {
+
+            });
+         }
+      },
+      'click section#answerZone > button': function(event, instances) {
+         if(!instances.done) {
+            let removedWord = event.currentTarget.innerText;
+            let currentOrderedList = instances.orderedWords.get();
+            let newOrderedList = _.without(currentOrderedList, removedWord);
+            instances.orderedWords.set(newOrderedList);
+            let newShuffledList = instances.shuffledWords.get();
+            newShuffledList.push(removedWord);
+            instances.shuffledWords.set(newShuffledList);
+         }
+      },
+      'click #end': function() {
+         Session.set('browseSession', 'frontPage');
+         Session.set('username', '');
       }
    }
 );
--- 2025/engShuffle/server/main.js
+++ 2025/engShuffle/server/main.js
@@ -5,6 +5,14 @@ import { Mongo } from 'meteor/mongo';
 global.sentenceDB = new Mongo.Collection('sentenceDB');
 global.userProfileDB = new Mongo.Collection('userProfileDB');
 
+Meteor.publish('sentencesByLevel', function(level) {
+	return sentenceDB.find({level: level}, {fields: {sentence: 0, words: 0}});
+});
+
+Meteor.publish('userProfile', function(username) {
+	return userProfileDB.find({username: username}, {fields: {RTs: 0}});
+});
+
 // Process the sentences in the sentence liston the server side when the app
 // starts
 Meteor.startup(async function() {
@@ -36,3 +44,26 @@ Meteor.startup(async function() {
 		}
 	);
 });
+
+Meteor.methods(
+	{
+		registerUser: function() {
+			userProfileDB.upsertAsync({username: 'TYC'}, 
+				{$setOnInsert: {level: 'beginner', score: 0, meanRT: 0, RTs: []}});
+		},
+		recordAns: async function(record) {
+			let targetSentence = await sentenceDB.findOneAsync({_id: record.sentenceID});
+			let targetLength = targetSentence.words.length;
+			let resArray = new Array(targetLength);
+			let newScore = 0;
+			for(let index = 0 ; index < targetLength ; index++) {
+				if(record.answer[index] === targetSentence.words[index]) {
+					newScore++;
+					resArray[index] = true;
+				}
+			}
+			console.log(newScore);
+			console.log(resArray);
+		}
+	}
+);
